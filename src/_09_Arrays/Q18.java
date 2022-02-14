ecf1a609e5cfde0ef69d5e239dc5f7f885bf5a08
Omer Tufan <omertufanca@gmail.com>
2022-02-14 15:57:16 +0100

    14.02.22

--- src/_09_Arrays/Q18.java
+++ src/_09_Arrays/Q18.java
@@ -2,6 +2,7 @@ package _09_Arrays;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Arrays;
 public class Q18 {
 
 	public static void main(String[] args)
@@ -9,6 +10,43 @@ public class Q18 {
 		 // kullanıcıdan alınan bir  String'deki herbir  karakterin adedini bir array içinde yazdıran bir java programı yazın.
         // String  "Javacilar cook afilli" ise cevap şöyle olmalıdır:
         // { =2, a=3, r=1, c=1, v=1, o=2, f=1, l=2, J=1, i=2}
+		
+		String str="Javacilar cook afilli";
+		String str2="";
+		for (int i = 0; i < str.length(); i++) {
+		if(!str2.contains(str.substring(i, i+1))) {
+			str2+=str.substring(i, i+1);
+		}
+			
+		}
+		
+		
+		String arr[]=str.split("");
+		String arr2[]=str2.split("");
+		
+		System.out.println(Arrays.toString(arr));
+		System.out.println(Arrays.toString(arr2));
+		
+		System.out.print("{");
+		int toplam;
+		for (int i = 0; i < arr2.length; i++) {
+		toplam=0;
+		
+			for (int j = 0; j < arr.length; j++) {
+				
+				if (arr[i].contains(arr[j])) {
+					toplam++;
+					
+				}
+								
+			}
+			
+			System.out.print(arr[i]+" = "+ toplam+ " ");
+		}
+		
+		
+		System.out.println("}");
+		
 		
 		
 	}
